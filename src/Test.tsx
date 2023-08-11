4930c71f28c0dfea9cb1fa8d8d9d3ce9b591b7d0
Kyle Larson <aaaknot30@yahoo.com>
2023-08-11 12:53:59 -0600

    cc

---
+++ src/Test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const last = (arr: Array<number>) => {
+  return arr[arr.length - 1];
+}
+
+const l = last([1, 2, 3]);
+
+
+function Test() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Test
